4541d256789031162721e74fbd904ad3495c9d82
MarkelRoman05 <markromagonz@gmail.com>
2025-03-26 16:52:39 +0100

    LoginView almost finished

--- app/src/main/java/com/zukbadakizu/talk2sign/MainView.java
+++ app/src/main/java/com/zukbadakizu/talk2sign/MainView.java
@@ -15,10 +15,6 @@ public class MainView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_view);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
